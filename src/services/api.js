f4b5782ad26e3787b98aa7a872555cb010d17268
Priyanshu Raj <8581priyanshu@gmail.com>
2026-01-03 02:14:42 +0530

    Use env-based API URL

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
  // baseURL: 'http://localhost:5010/api',
- baseURL: import.meta.env.VITE_BACKEND_BASE_URL,
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
